5417c58bea063a746bc36269199a08577b7d2ce7
viotalJiplk <viotal.varecha@outlook.com>
2023-10-07 19:04:05 +0200

    401 errors -> 403

--- src/routes/team/create.py
+++ src/routes/team/create.py
@@ -10,9 +10,9 @@ class createTeam(Resource):
     @postJson
     def post(self, data, authResult):
         if("game_id" not in data or "name" not in data):
-            return {"state": 1, "msg": "Missing game_id or name."}, 401
+            return {"state": 1, "msg": "Missing game_id or name."}, 403
         if("nick" not in data or "rank" not in data or "max_rank" not in data):
-            return {"state": 1, "msg": "Missing nick, rank, or max_rank of capitain."}, 401
+            return {"state": 1, "msg": "Missing nick, rank, or max_rank of capitain."}, 403
         user = UserModel.getById(authResult["userId"])
         if user is None:
             return {"kind": "JOIN", "msg": "User is not in database."}, 404
@@ -22,6 +22,6 @@ class createTeam(Resource):
         team = TeamModel.create(name=data["name"], gameId=data["game_id"], userId=authResult["userId"], nick=data["nick"], rank=data["rank"], maxRank=data["max_rank"])
         
         if team is None:
-            return {"kind": "JOIN", "msg": "Team full or you are in another team for this game."}, 401
+            return {"kind": "JOIN", "msg": "Team full or you are in another team for this game."}, 403
         
         return {"teamId": team.teamId}, 200
