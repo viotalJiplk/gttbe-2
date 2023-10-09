de2e8cee683be23ccf98437f982a8172109a0a0f
viotalJiplk <viotal.varecha@outlook.com>
2023-10-09 18:17:06 +0200

    fix import GameModel in teams/id

--- src/routes/team/id.py
+++ src/routes/team/id.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, request
 
 from models.team import TeamModel
+from models.game import GameModel
 from utils.jws import jwsProtected
 from functools import wraps
 from utils.utils import postJson
