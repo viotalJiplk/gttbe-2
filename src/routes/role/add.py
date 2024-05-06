5b337c07d071224b15d1be527be4d060ece1f68f
viotalJiplk <viotal.varecha@outlook.com>
2024-05-06 21:19:30 +0200

    fix(role): only game admin can manage role

--- src/routes/role/add.py
+++ src/routes/role/add.py
@@ -8,7 +8,7 @@ class AddRole(Resource):
 
     @jwsProtected()
     @postJson
-    @getRole(roleArray=["admin"])
+    @getRole(roleArray=["admin"], optional=False)
     def post(self, data, authResult, hasRole):
         if("game_id" not in data or "user_id" not in data or "role" not in data):
             return {"kind":"ROLE", "msg": "Missing game_id or user_id or role in data."}, 401
