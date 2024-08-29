c5d3d09ca492112de53517e375282bf9e42df05e
viotalJiplk <viotal.varecha@outlook.com>
2024-08-29 14:15:49 +0200

    feat(Observer): move to dict

--- src/shared/utils/attributesObserver.py
+++ src/shared/utils/attributesObserver.py
@@ -1,54 +1,81 @@
-class AttributesObserver:
+class AttributesObserver(dict):
     def __init__(self):
-        self.observerDict = {
+        self.__observerDict = {
             "init": {},
             "initAll": [],
             "update": {},
             "updateAll": [],
             "delete": {},
             "deleteAll": [],
+            "read": {},
+            "readAll": [],
         }
 
     def __setattr__(self, name, value):
-        if hasattr(self, "observerDict"):
-            if hasattr(self, name):
-                if hasattr(self.observerDict["update"], name):
-                    for x in self.observerDict["update"][name]:
-                        x(name, value)
-                for x in self.observerDict["updateAll"]:
-                    x(name, value)
-            else:
-                if hasattr(self.observerDict["init"], name):
-                    for x in self.observerDict["init"][name]:
-                        x(name, value)
-                for x in self.observerDict["initAll"]:
-                    x(name, value)
+        self.__setitem__(name, value)
 
-        super().__setattr__(name, value)
+    def __setitem__(self, key, value):
+        if hasattr(self, "__observerDict"):
+            if super().__getitem__(name):
+                if hasattr(self.__observerDict["update"], key):
+                    for x in self.__observerDict["update"][key]:
+                        x(key, value)
+                for x in self.__observerDict["updateAll"]:
+                    x(key, value)
+            else:
+                if hasattr(self.__observerDict["init"], key):
+                    for x in self.__observerDict["init"][key]:
+                        x(key, value)
+                for x in self.__observerDict["initAll"]:
+                    x(key, value)
+
+        super().__setitem__(key, value)
 
     def __delattr__(self, name):
-        if hasattr(self, "observerDict"):
-            if hasattr(self, name):
-                if hasattr(self.observerDict["delete"], name):
-                    for x in self.observerDict["delete"][name]:
-                        x(name, value)
-                for x in self.observerDict["deleteAll"]:
-                    x(name, value)
-        super().__delattr__(name, value)
+        self.__delitem__(name)
+
+    def __delitem__(self, key):
+        if hasattr(self, "__observerDict"):
+            if super().__getitem__(name):
+                if hasattr(self.__observerDict["delete"], key):
+                    for x in self.__observerDict["delete"][key]:
+                        x(key, value)
+                for x in self.__observerDict["deleteAll"]:
+                    x(key, value)
+        super().__delitem__(key, value)
+
+    def __getattr__(self, name):
+        if name == "__observerDict":
+            return self.__getattribute__(name)
+        try:
+            return self.__getitem__(name)
+        except KeyError as e:
+            raise AttributeError(e.args)
+
+    def __getitem__(self, key):
+        if hasattr(self, "__observerDict"):
+            if super().__getitem__(key):
+                if hasattr(self.__observerDict["read"], key):
+                    for x in self.__observerDict["read"][key]:
+                        x(key)
+                for x in self.__observerDict["readAll"]:
+                    x(key)
+        return super().__getitem__(key)
+
 
     def register(self, function, attrName, observerType):
         if observerType == "init" or observerType == "all":
             if attrName != None:
-                self.observerDict["init"][attrName].append(function)
+                self.__observerDict["init"][attrName].append(function)
             else:
-                self.observerDict["initAll"].append(function)
+                self.__observerDict["initAll"].append(function)
         if observerType == "update" or observerType == "all":
             if attrName != None:
-                self.observerDict["update"][attrName].append(function)
+                self.__observerDict["update"][attrName].append(function)
             else:
-                self.observerDict["updateAll"].append(function)
+                self.__observerDict["updateAll"].append(function)
         if observerType == "delete" or observerType == "all":
             if attrName != None:
-                self.observerDict["delete"][attrName].append(function)
+                self.__observerDict["delete"][attrName].append(function)
             else:
-                self.observerDict["deleteAll"].append(function)
+                self.__observerDict["deleteAll"].append(function)
