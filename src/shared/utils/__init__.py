9311d260d68cf950760e26178a8eb2e4cf8d1902
viotalJiplk <viotal.varecha@outlook.com>
2024-11-01 09:43:17 +0100

    feat(shared): move shared to submodule

--- src/shared/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .db import DatabaseError, dbConn, fetchOneWithNames, fetchAllWithNames
-from .dbSync import ObjectDbSync
-from .permissionList import perms
-from .configLoader import config
-from .generator import genState
-from .logging import getLogger, defaultLogger
-from .configLoader import config
-from .generator import genState
-from .date import fromTimeDelta
